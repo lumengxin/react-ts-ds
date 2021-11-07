fbb53d3bfed1521c1dee458b88fc0cbab34f02ee
lumengxin <empyr935@gmail.com>
2021-11-07 19:04:17 +0800

    context

--- src/components/Robot.tsx
+++ src/components/Robot.tsx
@@ -1,19 +1,27 @@
-import React  from 'react';
-import styles from './Robot.module.css'
+import React from "react"
+import styles from "./Robot.module.css"
+import { AppContext } from "../index"
 
 interface RobotProps {
-  id: number;
-  name: string;
+  id: number
+  name: string
   email: string
 }
 
-const Robot: React.FC<RobotProps> = ({id,name,email}) => {
+const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
   return (
-    <div className={styles.robotContainer}>
-      <h1>{id}</h1>
-      <p>{name}</p>
-      <p>{email}</p>
-    </div>
+    <AppContext.Consumer>
+      {(value) => {
+        return (
+          <div className={styles.robotContainer}>
+            <h1>{id}</h1>
+            <p>{name}</p>
+            <p>{email}</p>
+            <p>作者：{value.username}</p>
+          </div>
+        )
+      }}
+    </AppContext.Consumer>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css'; // 全局样式
-import reportWebVitals from './reportWebVitals';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css" // 全局样式
+import reportWebVitals from "./reportWebVitals"
+import App from "./App"
+
+const defaultValue = {
+  username: "xin",
+}
+
+export const AppContext = React.createContext(defaultValue)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppContext.Provider value={defaultValue}>
+      <App />
+    </AppContext.Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
