e932a438ad71aaa4e18d573c687dd8d4bc84a12c
lumengxin <empyr935@gmail.com>
2021-11-07 21:06:58 +0800

    高阶组件

--- src/components/Robot.tsx
+++ src/components/Robot.tsx
@@ -1,48 +1,29 @@
-import React, {useContext} from "react"
+import React, { useContext } from "react"
 import styles from "./Robot.module.css"
 import { AppContext, AppSetStateContext } from "../AppState"
+import { withAddToCart } from "./AddToCard"
 
-interface RobotProps {
+export interface RobotProps {
   id: number
   name: string
   email: string
+  addToCart: (id, name) => void
 }
 
-const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
+const Robot: React.FC<RobotProps> = ({ id, name, email, addToCart }) => {
   const value = useContext(AppContext)
-  const setState = useContext(AppSetStateContext)
-
-  const addToCart = () => {
-    if(setState) {
-      setState(state => {
-        return {
-          ...state,
-          shoppingCart: {
-            items: [
-              ...state.shoppingCart.items,
-              {id, name}
-            ]
-          }
-        }
-      })
-    }
-  }
 
   return (
-    // <AppContext.Consumer>
-    //   {(value) => {
-    //     return (
-          <div className={styles.robotContainer}>
-            <h1>{id}</h1>
-            <p>{name}</p>
-            <p>{email}</p>
-            <p>作者：{value.username}</p>
-            <button type="button" onClick={addToCart}>加入购物车</button>
-          </div>
-    //     )
-    //   }}
-    // </AppContext.Consumer>
+    <div className={styles.robotContainer}>
+      <h1>{id}</h1>
+      <p>{name}</p>
+      <p>{email}</p>
+      <p>作者：{value.username}</p>
+      <button type="button" onClick={() => addToCart(id, name)}>
+        加入购物车
+      </button>
+    </div>
   )
 }
 
-export default Robot
+export default withAddToCart(Robot)
