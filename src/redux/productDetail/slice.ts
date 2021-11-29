acd5634124776599fb80ae0bcb505d15464f755b
lumengxin <empyr935@gmail.com>
2021-11-29 22:29:59 +0800

    省略异步控制流

--- src/redux/productDetail/slice.ts
+++ src/redux/productDetail/slice.ts
@@ -13,35 +13,51 @@ const initialState: ProductDetailState = {
 	data: null
 }
 
+export const getAsyncProductDetail = createAsyncThunk(
+	'productDetail/getAsyncProductDetail',
+	async (id: string, thunkApi) => {
+		// thunkApi.dispatch(productDetailSlice.actions.fetchStart())
+		// try {
+		// 	const { data } = await axios.get('/detail.json')
+		// 	thunkApi.dispatch(productDetailSlice.actions.fetchSuccess(data))
+		// } catch (error: any) {
+		// 	thunkApi.dispatch(productDetailSlice.actions.fetchFail(error))
+		// }
+		const { data } = await axios.get('/detail.json')
+		return data
+	}
+)
+
 export const productDetailSlice = createSlice({
 	name: 'productDetail',
 	initialState,
-	reducers: {
-		fetchStart: (state) => {
-			// return { ...state, loading: true };
+	reducers: {},
+	// reducers: {
+	// 	fetchStart: (state) => {
+	// 		state.loading = true
+	// 	},
+	// 	fetchSuccess: (state, action) => {
+	// 		state.data = action.payload
+	// 		state.loading = false
+	// 		state.error = null
+	// 	},
+	// 	fetchFail: (state, action: PayloadAction<string | null>) => {
+	// 		state.loading = false
+	// 		state.error = action.payload
+	// 	}
+	// }
+	extraReducers: {
+		[getAsyncProductDetail.pending.type]: (state) => {
 			state.loading = true
 		},
-		fetchSuccess: (state, action) => {
+		[getAsyncProductDetail.fulfilled.type]: (state, action) => {
 			state.data = action.payload
 			state.loading = false
 			state.error = null
 		},
-		fetchFail: (state, action: PayloadAction<string | null>) => {
-			state.loading = false
+		[getAsyncProductDetail.rejected.type]: (state, action: PayloadAction<string | null>) => {
+			state.loading = true
 			state.error = action.payload
 		}
 	}
 })
-
-export const getAsyncProductDetail = createAsyncThunk(
-	'productDetail/getAsyncProductDetail',
-	async (id: string, thunkApi) => {
-		thunkApi.dispatch(productDetailSlice.actions.fetchStart())
-		try {
-			const { data } = await axios.get('/detail.json')
-			thunkApi.dispatch(productDetailSlice.actions.fetchSuccess(data))
-		} catch (error: any) {
-			thunkApi.dispatch(productDetailSlice.actions.fetchFail(error))
-		}
-	}
-)
