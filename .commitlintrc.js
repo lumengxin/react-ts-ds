88aeef91253fbff4bc4f07e4752eed06a679cb62
lumengxin <empyr935@gmail.com>
2021-12-04 12:15:14 +0800

    ci(config): ci(config): ci(config): ci(config): commit-lint生效配置测试

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,4 +1,67 @@
 module.exports = {
-	extends: ['cz']
-	// extends: ['@commitlint/config-conventional', 'cz']
+	extends: ['cz'],
+	// extends: ['@commitlint/config-conventional', 'cz'],
+	rules: {
+		// Header
+		'header-max-length': [2, 'always', 200],
+		// <type>枚举
+		'type-enum': [
+			2,
+			'always',
+			[
+				'feat',
+				'fix',
+				'style',
+				'docs',
+				'refactor',
+				'perf',
+				'ci',
+				'build',
+				'WIP',
+				'revert',
+				'test',
+				'chore'
+			]
+		],
+		// <type> 不能为空
+		'type-empty': [2, 'never'],
+		// <type> 格式 小写
+		'type-case': [2, 'always', 'lower-case'],
+		// <scope> 不能为空
+		'scope-empty': [2, 'never'],
+		// <scope> 格式 小写
+		'scope-case': [2, 'always', 'lower-case'],
+		'scope-enum': [0], // level: disabled
+		// 'function-rules/scope-enum': [
+		//   2, // level: error
+		//   'always',
+		//   (parsed) => {
+		//     const allowedScopes = ['@foo/bar'];
+		//     if (!parsed.scope || allowedScopes.includes(parsed.scope)) {
+		//       return [true];
+		//     }
+
+		//     return [false, `scope must be one of ${allowedScopes.join(', ')}`];
+		//   },
+		// ],
+		// <subject> 不能为空
+		'subject-empty': [2, 'never'],
+		// <subject> 以.为结束标志
+		'subject-full-stop': [2, 'never', '.'],
+		// <subject> 格式
+		// 可选值
+		// 'lower-case' 小写 lowercase
+		// 'upper-case' 大写 UPPERCASE
+		// 'camel-case' 小驼峰 camelCase
+		// 'kebab-case' 短横线 kebab-case
+		// 'pascal-case' 大驼峰 PascalCase
+		// 'sentence-case' 首字母大写 Sentence case
+		// 'snake-case' 下划线 snake_case
+		// 'start-case' 所有首字母大写 start-case
+		'subject-case': [2, 'never', []],
+		// <body> 以空行开头
+		'body-leading-blank': [1, 'always'],
+		// <footer> 以空行开头
+		'footer-leading-blank': [1, 'always']
+	}
 }
