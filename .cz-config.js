82ece0a690597a9abf6327ba3155c3cb203c3a9d
lumengxin <empyr935@gmail.com>
2021-12-04 15:17:48 +0800

    chore(business): test

--- .cz-config.js
+++ .cz-config.js
@@ -2,17 +2,17 @@
 module.exports = {
 	// 修改性质
 	types: [
-		{ value: ':art: feat', name: ' 🎨 feat: 新功能' },
-		{ value: ':bug: fix', name: ' 🐛 fix: 修复bug' },
-		{ value: ':lipstick: style', name: ' 💄 style: 代码格式（空格、分号等）' },
-		{ value: ':memo: docs', name: ' 📝 docs: 文档修改' },
-		{ value: ':recycle: refactor', name: ' ♻️ refactor: 重构（非feat、非fix）' },
-		{ value: ':zap: perf', name: ' ⚡️ perf: 提高性能' },
-		{ value: ':green_heart: ci', name: ' 💚 ci: 持续集成的配置文件或脚本的修改' },
-		{ value: ':construction_worker: build', name: ' 👷 build: 构建工具或外部依赖包的修改' },
-		{ value: 'WIP', name: ' 👨🏻‍🚀 WIP: 进行中的工作' },
-		{ value: 'revert', name: ' 🌚 revert: 撤销某次提交' },
-		{ value: 'test', name: ' 🦄 test: 添加缺少的测试' },
+		{ value: ':art: feat', name: '🎨feat: 新功能' },
+		{ value: ':bug: fix', name: '🐛fix: 修复bug' },
+		{ value: ':lipstick: style', name: '💄style: 代码格式（空格、分号等）' },
+		{ value: ':memo: docs', name: '📝docs: 文档修改' },
+		{ value: ':recycle: refactor', name: '♻️refactor: 重构（非feat、非fix）' },
+		{ value: ':zap: perf', name: '⚡️perf: 提高性能' },
+		{ value: ':green_heart: ci', name: '💚ci: 持续集成的配置文件或脚本的修改' },
+		{ value: ':construction_worker: build', name: '👷build: 构建工具或外部依赖包的修改' },
+		{ value: 'WIP', name: '👨🏻‍🚀WIP: 进行中的工作' },
+		{ value: 'revert', name: '🌚revert: 撤销某次提交' },
+		{ value: 'test', name: '🦄test: 添加缺少的测试' },
 		{
 			value: 'chore',
 			name: 'chore: 对构建过程或辅助工具和库的更改(不影响源文件、测试用例的其他操作)'
