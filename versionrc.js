802091f61b5223c70b9ad75b736e18e03db5e524
lumengxin <empyr935@gmail.com>
2021-12-04 16:01:02 +0800

    :bug: fix(business): bug修复

--- versionrc.js
+++ versionrc.js
@@ -8,20 +8,20 @@ module.exports = {
 	// types为Conventional Commits标准中定义，目前支持
 	// https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional
 	types: [
-		{ type: ':art: feat', section: ' 🎨 feat: 新功能' },
-		{ type: ':bug: fix', section: ' 🐛 fix: 修复bug' },
-		{ type: ':lipstick: style', section: ' 💄 style: 代码格式（空格、分号等）' },
-		{ type: ':memo: docs', section: ' 📝 docs: 文档修改' },
-		{ type: ':recycle: refactor', section: ' ♻️ refactor: 重构（非feat、非fix）' },
-		{ type: ':zap: perf', section: ' ⚡️ perf: 提高性能' },
-		{ type: ':green_heart: ci', section: ' 💚 ci: 持续集成的配置文件或脚本的修改' },
-		{ type: ':construction_worker: build', section: ' 👷 build: 构建工具或外部依赖包的修改' },
-		{ type: 'WIP', section: ' 👨🏻‍🚀 WIP: 进行中的工作' },
-		{ type: 'revert', section: ' 🌚 revert: 撤销某次提交' },
-		{ type: 'test', section: ' 🦄 test: 添加缺少的测试' },
+		{ type: ':art: feat', section: '🎨feat: 新功能' },
+		{ type: ':bug: fix', section: '🐛fix: 修复bug' },
+		{ type: ':lipstick: style', section: '💄style: 代码格式（空格、分号等）' },
+		{ type: ':memo: docs', section: '📝docs: 文档修改' },
+		{ type: ':recycle: refactor', section: '♻️refactor: 重构（非feat、非fix）' },
+		{ type: ':zap: perf', section: '⚡️perf: 提高性能' },
+		{ type: ':green_heart: ci', section: '💚ci: 持续集成的配置文件或脚本的修改' },
+		{ type: ':construction_worker: build', section: '👷build: 构建工具或外部依赖包的修改' },
+		{ type: ':seedling: WIP', section: '🌱WIP: 进行中的工作' },
+		{ type: ':rewind: revert', section: '⏪️revert: 撤销某次提交' },
+		{ type: ':test_tube: test', section: '🧪test: 添加缺少的测试' },
 		{
-			type: 'chore',
-			section: ' 🐢 chore: 对构建过程或辅助工具和库的更改(不影响源文件、测试用例的其他操作)'
+			type: ':dizzy: chore',
+			section: '💫chore: 对构建过程或辅助工具和库的更改(不影响源文件、测试用例的其他操作)'
 		}
 	]
 	// 需要server-version更新版本号的文件
